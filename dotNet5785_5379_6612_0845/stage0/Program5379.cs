2239d71043adb0f413e7389661481e175f1202c2
RivkiPerkoff <Hand4d24@mbjcomp.org.il>
2024-10-31 20:27:45 +0200

    Initial Code + Welcome functions

---
+++ dotNet5785_5379_6612_0845/stage0/Program5379.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace targil0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome5379();
+            Welcome6612();
+            Welcome0845();
+            Console.ReadKey();
+
+        }
+
+        static partial void Welcome6612();
+        static partial void Welcome0845();
+
+        private static void Welcome5379()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"{name}, welcome to my first console application");
+        }
+    }
+}
