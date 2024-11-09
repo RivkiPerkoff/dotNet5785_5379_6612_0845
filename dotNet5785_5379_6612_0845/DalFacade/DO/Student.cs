f5033295ff75322b7103fe30531e3fb0e119f93b
avigail beckerman <beckermanavigail@gmail.com>
2024-11-09 22:45:41 +0200

    מוצש

---
+++ dotNet5785_5379_6612_0845/DalFacade/DO/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace DO;
+
+public record Student
+(
+int Id,
+string Name,
+DateTime RegistrationDate,
+string? Alias = null,
+bool IsActive = false,
+DateTime? BirthDate = null
+)
+{
+    /// <summary>
+    /// Default constructor for stage 3
+    /// </summary>
+    public Student() : this(0, "", DateTime.Now, null, false, null) { }
+}
---
+++ dotNet5785_5379_6612_0845/DalList/Config.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Dal;
+internal static class Config
+{
+    internal const int StartCourseId = 1000;
+    private static int s_nextCourseId = StartCourseId;
+    internal static int NextCourseId { get => s_nextCourseId++; }
+    //...
+    internal static DateTime Clock { get; } = DateTime.Now;
+    //...
+    internal static void Reset()
+    {
+       // s_nextCourseId = StartCourseId;
+        //...
+       // Clock = DateTime.Now;
+        //...
+    } 
+}
