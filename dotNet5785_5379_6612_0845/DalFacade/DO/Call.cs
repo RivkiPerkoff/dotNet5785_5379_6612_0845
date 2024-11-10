b8629b4603228ecd32b4c5542ea394019f6ee914
avigail beckerman <beckermanavigail@gmail.com>
2024-11-10 23:18:30 +0200

    חלק

--- dotNet5785_5379_6612_0845/DalFacade/DO/Call.cs
+++ dotNet5785_5379_6612_0845/DalFacade/DO/Call.cs
@@ -4,5 +4,15 @@ namespace DO
 {
     internal class Call
     {
+        int IdCall,
+        string CallDescription = null,
+        string CallAddress = "",
+        double CallLatitude,
+        double CallLongitude,
+        internal static DataTime OpeningTime,
+
+
+
+
     }
 }
