dd28b5806005274d781a03a329532ed237ca6f30
RivkiPerkoff <Hand4d24@mbjcomp.org.il>
2024-11-10 23:00:10 +0200

    עד כאן הוספתי את CALL

--- dotNet5785_5379_6612_0845/DalFacade/DO/Enumes.cs
+++ dotNet5785_5379_6612_0845/DalFacade/DO/Enumes.cs
@@ -1,10 +1,10 @@
 ﻿
 namespace DO
 {
-    internal class Enumes
+    internal static class Enumes
     {
         public enum AreaActivity { Jerusalem, Tel_Aviv, Haifa, Beer_Sheva, Rishon_Lezion, Beit_Shemesh, Bnei_Brak, Beitar }
         public enum ModelCar { Kia, Toyota, Tesla, Mitsubishi, Subaru, Mercedes, Jaguar, Bentley, Volvo, BMW, Jaecoo, Mazda }
-        public enum CarColor { Black,Grey,White,Red,Blue,Brown,Yellow }
+        public enum CarColor { Black, Grey, White, Red, Blue, Brown, Yellow }
     }
 }
