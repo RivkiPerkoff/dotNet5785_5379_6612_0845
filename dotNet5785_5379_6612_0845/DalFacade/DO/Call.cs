ac15119ebc1e16c2de62e05fbac35e76817ecfbd
RivkiPerkoff <Hand4d24@mbjcomp.org.il>
2024-11-10 23:35:02 +0200

    Call

--- dotNet5785_5379_6612_0845/DalFacade/DO/Call.cs
+++ dotNet5785_5379_6612_0845/DalFacade/DO/Call.cs
@@ -4,15 +4,11 @@ namespace DO
 {
     internal class Call
     {
-        int IdCall,
-        string CallDescription = null,
-        string CallAddress = "",
-        double CallLatitude,
-        double CallLongitude,
-        internal static DataTime OpeningTime,
-
-
-
-
+        int IdCall;
+        string CallDescription = null;
+        string CallAddress = "";
+        double CallLatitude;
+        double CallLongitude;
+        internal static DateTime OpeningTime { get; set; } = DateTime.Now;
     }
 }
