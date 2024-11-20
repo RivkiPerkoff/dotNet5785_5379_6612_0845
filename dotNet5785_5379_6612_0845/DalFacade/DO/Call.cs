6cc328212fbabca6060add747b94aefbab4d7bf4
tamarminhahar <tamarminhahar@gmail.com>
2024-11-21 00:36:13 +0200

    ,m

--- dotNet5785_5379_6612_0845/DalFacade/DO/Call.cs
+++ dotNet5785_5379_6612_0845/DalFacade/DO/Call.cs
@@ -2,13 +2,13 @@
 
 namespace DO
 {
-    internal class Call
+    internal record Call
     {
         int IdCall;
-        string CallDescription = null;
-        string CallAddress = "";
-        double CallLatitude;
-        double CallLongitude;
+        string? CallDescription = null;
+        string? CallAddress = null;
+        double? CallLatitude=0;
+        double? CallLongitude=0;
         internal static DateTime OpeningTime { get; set; } = DateTime.Now;
     }
 }
--- dotNet5785_5379_6612_0845/DalList/VolunteerImplementation.cs
+++ dotNet5785_5379_6612_0845/DalList/VolunteerImplementation.cs
@@ -4,7 +4,7 @@ using DalApi;
 using DO;
 using System.Collections.Generic;
 
-internal class VolunteerImplementation : IVolunteer
+internal class VolunteerImplementation/* : IVolunteer*/
 {
     public void Create(Volunteer item)
     {
@@ -31,9 +31,21 @@ internal class VolunteerImplementation : IVolunteer
         throw new NotImplementedException();
     }
 
-    public void Update(Volunteer item)
-    {
-        throw new NotImplementedException();
-    }
+    //    public void Update(Volunteer item)
+    //    {
+    //        // חפש אם קיים אובייקט עם ה-ID במאגר הנתונים
+    //        var existingItem = _repository.GetVolunteerById(item.VolunteerId);
+
+    //        if (existingItem == null)
+    //        {
+    //            throw new InvalidOperationException($"אובייקט מסוג Volunteer עם ID {item.VolunteerId} לא קיים.");
+    //        }
+
+    //        // אם האובייקט קיים, מחק את הישן והוסף את החדש
+    //        _repository.RemoveVolunteer(item.VolunteerId);
+    //        _repository.AddVolunteer(item);
+    //    }
+
+
 }
 
