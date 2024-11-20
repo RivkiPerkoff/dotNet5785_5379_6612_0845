77c5253b886b4b509f18d9cf8ef4e793405656f7
avigail beckerman <beckermanavigail@gmail.com>
2024-11-21 00:37:13 +0200

    שתי הפונקציות שלי

---
+++ dotNet5785_5379_6612_0845/DalList/VolaunteerImplementation.cs
@@ -0,0 +1,45 @@
+﻿
+namespace Dal;
+using DalApi;
+using DO;
+using System.Collections.Generic;
+
+public class VolaunteerImplementation : IVolunteer
+
+{
+    public void Create(Volunteer item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteAll()
+    {
+    
+            items.Clear();
+  
+    }
+
+    public Volunteer? Read(int id)
+    {
+        return items.FirstOrDefault(item => idSelector(item) == id);
+    }
+
+    public List<Volunteer> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Volunteer item)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+{
+    items.Clear();
+   }
