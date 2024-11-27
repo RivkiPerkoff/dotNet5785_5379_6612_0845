ededd443cd76fafed50982ef34b1dacb1d757d7a
tamarminhahar <tamarminhahar@gmail.com>
2024-11-27 19:59:15 +0200

    program

--- dotNet5785_5379_6612_0845/DalTest/Program.cs
+++ dotNet5785_5379_6612_0845/DalTest/Program.cs
@@ -7,8 +7,8 @@ namespace DalTest;
 
 internal class Program
 {
-    private static IVolunteer? s_dalVolunteer = new VolunteerImplementation(); // stage 1
-    private static ICall? s_dalCall = new CallImplementation(); // stage 1
+    private static IVolunteer? s_dalVolunteer = new VolunteerImplementation(); 
+    private static ICall? s_dalCall = new CallImplementation(); 
     private static IAssignment? s_dalAssignment = new AssignmentImplementation(); // stage 1
     private static IConfig? s_dalConfig = new ConfigImplementation();
 
