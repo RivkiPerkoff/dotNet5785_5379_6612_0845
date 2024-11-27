cea1fcd1ae43f3ffe3e929b227f9577db5c69f89
RivkiPerkoff <Hand4d24@mbjcomp.org.il>
2024-11-27 22:00:58 +0200

    התיקון הכללי

--- dotNet5785_5379_6612_0845/DalFacade/DalApi/IConfig.cs
+++ dotNet5785_5379_6612_0845/DalFacade/DalApi/IConfig.cs
@@ -6,7 +6,9 @@ public interface IConfig
 {
     public  DateTime Clock { get; set; }
     public TimeSpan RiskRange { get; set; }
-    public int Create();
+    public int CreateAssignmentId();
+    public int CreateCallId();
+    public int CreateVolunteerId();
     public void Reset();
 
 }
--- dotNet5785_5379_6612_0845/DalList/Config.cs
+++ dotNet5785_5379_6612_0845/DalList/Config.cs
@@ -8,13 +8,24 @@ internal record Config
 
     public static int NextVolunteerId => s_nextVolunteerId++;
 
+    internal const int StartCallId = 1000;
+    private static int s_nextCallId = StartCallId;
+
+    public static int NextCallId => s_nextCallId++;
+
+    internal const int StartAssignmentId = 1000;
+    private static int s_nextAssignmentId = StartAssignmentId;
+    public static int NextAssignmentId => s_nextAssignmentId++;
+
     public static DateTime Clock { get;internal set; } = DateTime.Now;
 
     public static void Reset()
     {
         s_nextVolunteerId = StartVolunteerId;
+        s_nextCallId=StartCallId;
+        s_nextAssignmentId=StartAssignmentId;   
         Clock = DateTime.Now;
     }
 
-    internal static TimeSpan? RiskRange { get; set; }
+    internal static TimeSpan? RiskRange { get; set; } = new TimeSpan(1, 30, 0);
 }
