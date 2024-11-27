1953ce6e95ffb3a80361b7fe9f59046c193645eb
RivkiPerkoff <Hand4d24@mbjcomp.org.il>
2024-11-27 22:21:37 +0200

    כי

--- dotNet5785_5379_6612_0845/DalFacade/DalApi/IConfig.cs
+++ dotNet5785_5379_6612_0845/DalFacade/DalApi/IConfig.cs
@@ -5,7 +5,7 @@ using DO;
 public interface IConfig
 {
     public  DateTime Clock { get; set; }
-    public TimeSpan RiskRange { get; set; }
+    public TimeSpan? RiskRange { get; set; }
     public int CreateAssignmentId();
     public int CreateCallId();
     public int CreateVolunteerId();
