578f6f6087f06ae532239af0f59b55040c2e2da8
tamarminhahar <tamarminhahar@gmail.com>
2024-11-27 21:03:23 +0200

    צריך תיקונים

--- dotNet5785_5379_6612_0845/DalFacade/DO/Call.cs
+++ dotNet5785_5379_6612_0845/DalFacade/DO/Call.cs
@@ -1,30 +1,22 @@
-﻿
-namespace DO;
+﻿namespace DO;
+
 public record Call
 (
-    int IdCall,
-    string? CallDescription,
-    string? CallAddress,
-    double CallLatitude,
-    double CallLongitude,
-    DateTime OpeningTime,
-    DateTime MaxFinishTime,
-    TypeOfReading TypeOfReading
+    int IdCall = 0,
+    string? CallDescription = "",
+    string? CallAddress = "",
+    double CallLatitude = 0,
+    double CallLongitude = 0,
+    DateTime? OpeningTime = null,
+    DateTime? MaxFinishTime = null,
+    TypeOfReading TypeOfReading = TypeOfReading.Type1
 )
 {
-    public Call() : this(0, "", "", 0, 0, DateTime.Now, DateTime.Now, TypeOfReading.Type1) { }  // ערך ברירת מחדל עבור TypeOfReading
+    // קונסטרקטור מותאם אישית
+    public Call(int idCall, string? callDescription, string? callAddress, double callLatitude, double callLongitude, DateTime openingTime, DateTime maxFinishTime, TypeOfReading typeOfReading)
+        : this(idCall, callDescription, callAddress, callLatitude, callLongitude, (DateTime?)openingTime, (DateTime?)maxFinishTime, typeOfReading)
+    {
+    }
 }
 
-//{
-//    public Call(int idCall, string? callDescription, string? callAddress, double? callLatitude, double? callLongitude, DateTime openingTime, DateTime maxFinishTime, TypeOfReading TypeOfReading)
-//    {
-//        IdCall = idCall;
-//        CallDescription = callDescription;
-//        CallAddress = callAddress;
-//        CallLatitude = callLatitude;
-//        CallLongitude = callLongitude;
-//        OpeningTime = openingTime;
-//        MaxFinishTime = maxFinishTime;
-//        TypeOfReading TypeOfReading;
-//    }
-//}
+
