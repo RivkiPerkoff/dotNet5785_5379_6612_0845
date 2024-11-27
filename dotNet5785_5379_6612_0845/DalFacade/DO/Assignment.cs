77c1077e619ee071449485417917f7b728789824
tamarminhahar <tamarminhahar@gmail.com>
2024-11-27 02:35:18 +0200

    122

--- dotNet5785_5379_6612_0845/DalFacade/DO/Assignment.cs
+++ dotNet5785_5379_6612_0845/DalFacade/DO/Assignment.cs
@@ -10,24 +10,23 @@ public record Assignment
     int VolunteerId,
     DateTime EntryTimeForTreatment,
     DateTime? EndTimeForTreatment = null,
-    TerminationTypeTheTreatment? TerminationTypeTheTreatment = null
+    TypeOfEndTime TypeOfEndTime = TypeOfEndTime.treated  // משתמשים ב-enum הקיים
 )
 {
-    // קונסטרוקטור מותאם אישית - מאפשר יצירת אובייקט עם ערכים ברירתיים
+    // קונסטרוקטור ברירת מחדל עם ערכים בסיסיים
     public Assignment()
-        : this(0, 0, 0, DateTime.Now) { }
+        : this(0, 0, 0, DateTime.Now, null, TypeOfEndTime.treated) { }
 
     // קונסטרוקטור מותאם אישית נוסף
     public Assignment(DateTime randomTime, DateTime dateTime, FinishCallType finishCallType)
-        : this(0, 0, 0, randomTime)
+        : this(0, 0, 0, randomTime, null, TypeOfEndTime.treated)
     {
-   
         RandomTime = randomTime;
         DateTime = dateTime;
         FinishCallType = finishCallType;
     }
 
-    // תכונות נוספות
+    // תכונות נוספות שקשורות לקונסטרוקטור המותאם אישית
     public DateTime RandomTime { get; init; }
     public DateTime DateTime { get; init; }
     public FinishCallType FinishCallType { get; init; }
