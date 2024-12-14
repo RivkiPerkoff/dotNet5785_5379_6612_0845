480c7188002c1438a2aefbab06e459942830df96
tamarminhahar <tamarminhahar@gmail.com>
2024-12-14 23:54:24 +0200

    Chapter 7 - Adding a new class called DalXml to implement IDal

---
+++ dotNet5785_5379_6612_0845/DalXml/DalXml.cs
@@ -0,0 +1,25 @@
+﻿using DalApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal;
+sealed public class DalXml : IDal
+{
+    public IVolunteer Volunteer { get; } = new VolunteerImplementation();
+
+    public IAssignment Assignment { get; } = new AssignmentImplementation();
+
+    public ICall Call { get; } = new CallImplementation();
+
+    public IConfig Config { get; } = new ConfigImplementation();
+    public void ResetDB()
+    {
+        Volunteer.DeleteAll();
+        Assignment.DeleteAll();
+        Call.DeleteAll();
+        Config.Reset();
+    }
+}
