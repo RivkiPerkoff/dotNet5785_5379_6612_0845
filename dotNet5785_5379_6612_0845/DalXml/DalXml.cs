75dcf5af13e38139dc6fa24c04637e0fa28d386d
tamarminhahar <tamarminhahar@gmail.com>
2024-12-30 16:23:12 +0200

    char` 1&2

--- dotNet5785_5379_6612_0845/DalXml/DalXml.cs
+++ dotNet5785_5379_6612_0845/DalXml/DalXml.cs
@@ -11,8 +11,18 @@ namespace Dal;
 /// The <c>DalXml</c> class implements the <c>IDal</c> interface and provides access to the XML-based data access layers.
 /// It includes modules for managing volunteers, assignments, calls, and configuration settings.
 /// </summary>
-sealed public class DalXml : IDal
+sealed internal class DalXml : IDal
 {
+    // public static IDal Instance { get; } = new DalList();
+    private static class DalListHolder
+    {
+        // המשתנה הסטטי נוצר Lazy בצורה Thread Safe
+        internal static readonly IDal instance = new DalList();
+    }
+    // פרסום ה-Instance
+    public static IDal Instance => DalListHolder.instance;
+
+    private DalXml() { }
     /// <summary>
     /// Gets the implementation for managing volunteers.
     /// </summary>
