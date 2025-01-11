46f5d3846c1249d78cfbb9b67d046456e1b97b46
RivkiPerkoff <Hand4d24@mbjcomp.org.il>
2025-01-11 21:31:32 +0200

    Implementing the volunteer entity

--- dotNet5785_5379_6612_0845/BL/BO/Enums..cs
+++ dotNet5785_5379_6612_0845/BL/BO/Enums..cs
@@ -1,3 +1,14 @@
 ﻿
 
 namespace BL.BO;
+public enum Role
+{
+    Manager,
+    Volunteer
+}
+public enum DistanceType
+{
+    AirDistance,
+    RoadDistance,
+    WalkingDistance
+}
--- dotNet5785_5379_6612_0845/BL/BO/Volunteer.cs
+++ dotNet5785_5379_6612_0845/BL/BO/Volunteer.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
-namespace BL.BO
+namespace BL.BO;
+
+internal class Volunteer
 {
-    internal class Volunteer
-    {
-    }
+    public int Id { get; init; }
+    public string Name { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string? Password { get; set; }
+    public string? Address { get; set; }
+    double? VolunteerLatitude { get; set; }
+    double? VolunteerLongitude { get; set; }
+    Role Role { get; set; }
+    double? MaxDistance { get; set; }
+    public bool IsActive { get; set; }
+    DistanceType DistanceType { get; set; }
+    int TotalCallsHandled { get; set; }
+    int TotalCallsCanceled { get; set; }
+    int SelectedAndExpiredCalls { get; set; }
+    CallInProgress? callInProgress { get; set; }
+    public override string ToString() => this.ToStringProperty();
 }
