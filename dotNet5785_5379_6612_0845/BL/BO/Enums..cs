a900d46d698b8ea0c97e25a9944891721582c7b5
avigail beckerman <beckermanavigail@gmail.com>
2025-01-11 20:50:50 +0200

    Continued Chapter 6

---
+++ dotNet5785_5379_6612_0845/BL/BO/Enums..cs
@@ -0,0 +1,3 @@
+﻿
+
+namespace BL.BO;
--- dotNet5785_5379_6612_0845/BL/BO/OpenCallInList.cs
+++ dotNet5785_5379_6612_0845/BL/BO/OpenCallInList.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BL.BIApi;
-
-internal class IVolunteer
+namespace BL.BO
 {
-    public 
+    internal class OpenCallInList
+    {
+    }
 }
