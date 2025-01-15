99278f9419164a349597bb239c0ae2c71969e0c0
tamarminhahar <tamarminhahar@gmail.com>
2025-01-15 20:14:18 +0200

    create Icall interface(not finish)

--- dotNet5785_5379_6612_0845/BL/BO/ICall.cs
+++ dotNet5785_5379_6612_0845/BL/BO/ICall.cs
@@ -4,9 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BL.BO
+namespace BL.BO;
+public interface ICall
 {
-    internal interface Icall
-    {
-    }
+    int[] CallListAmount();
+
+    //לא גמור
+    CallInList RequestCallListRead( Enum toFiltering, object? filtering, Enum toSort);
+    void UpdateCallDetail(Call callObject);  
+    void DeleteCall(int id);  
+    void AddCall(Call callObject);
+    //לא גמור
+    ClosedCallInList RequestListOfClosedCallsVolunteer(int volunteerId, CallTypes? callType ,Enum a);
+    //לא גמור
+    ClosedCallInList RequestListOfOpenCallSelectedByvolunteer(int volunteerId, Enum? a, Enum? b);
+    void UpdateEndTreatment(int id, int idAsignment);
+    void UpdateCancelTreatment(int id, int idAsignment);
+    void ChoosingCallForTreatment(int id, int idAsignment);
+
 }
