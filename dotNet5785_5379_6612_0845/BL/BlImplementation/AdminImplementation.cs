f1bf42b2c89b8e07617ad9cac30512767c3e1088
tamarminhahar <tamarminhahar@gmail.com>
2025-01-29 00:51:19 +0200

    change the admin-imle

--- dotNet5785_5379_6612_0845/BL/BlImplementation/AdminImplementation.cs
+++ dotNet5785_5379_6612_0845/BL/BlImplementation/AdminImplementation.cs
@@ -4,39 +4,56 @@ using BL.Helpers;
 namespace BL.BlImplementation;
 internal class AdminImplementation : IAdmin
 {
-    public DateTime AdvanceClock(BO.TimeUnit timeUnit)
+    private readonly DalApi.IDal _dal = DalApi.Factory.Get;
+
+    public void AdvanceClock(BO.TimeUnit timeUnit)
     {
-       return ClockManager.UpdateClock(ClockManager.Now.AddMinutes(1));
-        throw new NotImplementedException();
+        // קבלת הזמן הנוכחי
+        DateTime currentClock = ClockManager.Now;
+
+        // יצירת הזמן החדש על פי יחידת הזמן
+        DateTime newClock = timeUnit switch
+        {
+            BO.TimeUnit.Minute => currentClock.AddMinutes(1),
+            BO.TimeUnit.Hour => currentClock.AddHours(1),
+            BO.TimeUnit.Day => currentClock.AddDays(1),
+            BO.TimeUnit.Month => currentClock.AddMonths(1),
+            BO.TimeUnit.Year => currentClock.AddYears(1),
+            _ => throw new ArgumentException("Invalid time unit", nameof(timeUnit))
+        };
+
+        ClockManager.UpdateClock(newClock);
     }
 
     public DateTime GetClock()
     {
         return ClockManager.Now;
-
-        throw new NotImplementedException();
     }
 
     public TimeSpan GetRiskTimeRange()
     {
-        throw new NotImplementedException();
-    }
-
-    public void InitializeDatabase()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void ResetDatabase()
-    {
-        throw new NotImplementedException();
+        // שימוש במשתנה _dal במקום Factory.Get
+        return _dal.Config.RiskRange
+               ?? throw new InvalidOperationException("RiskRange is not set in the configuration.");
     }
 
     public void SetRiskTimeRange(TimeSpan timeRange)
     {
-        throw new NotImplementedException();
+        // שימוש במשתנה _dal במקום Factory.Get
+        _dal.Config.RiskRange = timeRange;
     }
 
-    private readonly DalApi.IDal _dal = DalApi.Factory.Get;
+    public void InitializeDatabase()
+    {
+        DalTest.Initialization.DO();
+        ClockManager.UpdateClock(ClockManager.Now);
+    }
 
+    public void ResetDatabase()
+    {
+        // שימוש במשתנה _dal במקום Factory.Get
+        _dal.ResetDB();
+        DalTest.Initialization.DO();
+        ClockManager.UpdateClock(ClockManager.Now);
+    }
 }
