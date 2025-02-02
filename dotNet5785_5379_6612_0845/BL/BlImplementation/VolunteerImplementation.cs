0c33dfbaa187e3b8e5aafb57fce99513cb6a3910
tamarminhahar <tamarminhahar@gmail.com>
2025-02-02 02:12:39 +0200

    implemention ChoosingCallForTreatment

--- dotNet5785_5379_6612_0845/BL/BlImplementation/VolunteerImplementation.cs
+++ dotNet5785_5379_6612_0845/BL/BlImplementation/VolunteerImplementation.cs
@@ -202,7 +202,7 @@ internal class VolunteerImplementation : IVolunteer
             //TotalCallsHandled = doVolunteer.TotalCallsHandled,
             //TotalCallsCanceled = doVolunteer.TotalCallsCanceled,
             //SelectedAndExpiredCalls = doVolunteer.SelectedAndExpiredCalls,
-            //CallInProgress = doVolunteer.callInProgress 
+            //CallInProgress = doVolunteer.callInProgress
         };
     }
 
