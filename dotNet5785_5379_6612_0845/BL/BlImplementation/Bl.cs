e3ddbb869203cc0d65a08c9b34e36c38c94feec6
RivkiPerkoff <Hand4d24@mbjcomp.org.il>
2025-02-02 22:57:32 +0200

    Creating a BI department

---
+++ dotNet5785_5379_6612_0845/BL/BlImplementation/Bl.cs
@@ -0,0 +1,14 @@
+﻿
+
+using BL.BIApi;
+
+namespace BL.BlImplementation;
+
+internal class Bl : IBL
+{
+    public ICall Call { get; } = new CallImplementation();
+
+    public IVolunteer Volunteer { get; } = new VolunteerImplementation();
+
+    public IAdmin Admin { get; } = new AdminImplementation();
+}
