ab8d7646ebaa6d5794b172612ce5765430bc6846
RivkiPerkoff <Hand4d24@mbjcomp.org.il>
2025-05-20 01:15:18 +0300

    ch 6

--- dotNet5785_5379_6612_0845/PL/MainWindow.xaml.cs
+++ dotNet5785_5379_6612_0845/PL/MainWindow.xaml.cs
@@ -22,21 +22,21 @@ namespace PL
         public MainWindow()
         {
             InitializeComponent();
-
-            // רישום מתודה שתופעל כאשר השעון משתנה
-            s_bl.Admin.AddClockObserver(UpdateClockDisplay);
+            CurrentTime = s_bl.Admin.GetClock();
 
             // הצגת הזמן הנוכחי הראשוני
-            CurrentTime = s_bl.Admin.GetClock();
-        }
-        private void UpdateClockDisplay()
-        {
-            CurrentTime = s_bl.Admin.GetClock();
-        }
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
+            s_bl.Admin.AddClockObserver(clockObserver);
+            s_bl.Admin.AddConfigObserver(configObserver);
 
+            // לקרוא אותן בפעם הראשונה כדי לקבל את הערכים ההתחלתיים
+            clockObserver();
+            configObserver();
         }
+ 
+        //private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+
+        //}
         public DateTime CurrentTime
         {
             get { return (DateTime)GetValue(CurrentTimeProperty); }
@@ -63,22 +63,48 @@ namespace PL
         {
             s_bl.Admin.AdvanceClock(BL.BO.TimeUnit.Year);
         }
+        private void UpdateConfigButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                TimeSpan range = TimeSpan.FromDays(MaxYearRange * 365);
+                s_bl.Admin.SetRiskTimeRange(range);
 
-        //private void btnAddOneMinute_Click(object sender, RoutedEventArgs e)
-        //{
-        //    s_bl.Admin.AddClockObserver(BL.BO.TimeUnit.Minute);
-        //}
-        //private void btnAddOneHour_Click(object sender, RoutedEventArgs e)
-        //{
-        //    s_bl.Admin.AddClockObserver(BL.BO.TimeUnit.Hour);
-        //}
-        //private void btnAddOneDay_Click(object sender, RoutedEventArgs e)
-        //{
-        //    s_bl.Admin.AddClockObserver(BL.BO.TimeUnit.Day);
-        //}
-        //private void btnAddOneYear_Click(object sender, RoutedEventArgs e)
-        //{
-        //    s_bl.Admin.AddClockObserver(BL.BO.TimeUnit.Year);
-        //}
+                MessageBox.Show("משתנה התצורה עודכן בהצלחה!", "עדכון", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"אירעה שגיאה בעת עדכון משתנה התצורה: {ex.Message}", "שגיאה", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        public int MaxYearRange
+        {
+            get { return (int)GetValue(MaxYearRangeProperty); }
+            set { SetValue(MaxYearRangeProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaxYearRangeProperty =
+            DependencyProperty.Register("MaxYearRange", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+        private void clockObserver()
+        {
+            // מקבל את הזמן מה- BL ומעדכן את ה- CurrentTime
+            CurrentTime = s_bl.Admin.GetClock();
+        }
+
+        private void configObserver()
+        {
+            // מקבל את ערך משתנה התצורה ומעדכן אותו
+            MaxYearRange = s_bl.Admin.GetRiskTimeRange().Days / 365;
+        }
+
+        private void clockObserver(object sender, EventArgs e)
+        {
+            s_bl.Admin.GetClock();
+        }
+
+
+
     }
 }
