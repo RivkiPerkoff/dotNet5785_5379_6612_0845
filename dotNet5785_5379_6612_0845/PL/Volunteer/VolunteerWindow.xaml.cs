b2c08a66c8644b72b8584e18b59b6b85ae3894cc
RivkiPerkoff <Hand4d24@mbjcomp.org.il>
2025-05-25 03:41:17 +0300

    ch 9

--- dotNet5785_5379_6612_0845/PL/Volunteer/VolunteerWindow.xaml.cs
+++ dotNet5785_5379_6612_0845/PL/Volunteer/VolunteerWindow.xaml.cs
@@ -1,106 +1,116 @@
-﻿using System.Windows;
+﻿// VolunteerWindow.xaml.cs
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
-using BL.BO; // וודא שיש reference לפרויקט שבו נמצאת המחלקה Volunteer
+using BL.BIApi;
+using BL.BO;
 
-namespace BO.Volunteer;
+namespace PL.Volunteer
+{
+    public partial class VolunteerWindow : Window, INotifyPropertyChanged
+    {
+        private static readonly IBL s_bl = BlApi.Factory.Get();
 
-//public partial class VolunteerWindow : Window, INotifyPropertyChanged
-//{
-//    private Volunteer _volunteer;
-//    public Volunteer Volunteer
-//    {
-//        get => _volunteer;
-//        set
-//        {
-//            _volunteer = value;
-//            OnPropertyChanged(nameof(Volunteer));
-//        }
-//    }
+        private BL.BO.Volunteer _currentVolunteer;
+        public BL.BO.Volunteer CurrentVolunteer
+        {
+            get => _currentVolunteer;
+            set
+            {
+                _currentVolunteer = value;
+                OnPropertyChanged(nameof(CurrentVolunteer));
+            }
+        }
 
-//    private string _buttonText = "Add";
-//    public string ButtonText
-//    {
-//        get => _buttonText;
-//        set { _buttonText = value; OnPropertyChanged(nameof(ButtonText)); }
-//    }
+        private string _buttonText;
+        public string ButtonText
+        {
+            get => _buttonText;
+            set
+            {
+                _buttonText = value;
+                OnPropertyChanged(nameof(ButtonText));
+            }
+        }
+        public VolunteerWindow(int id = 0)
+        {
+            InitializeComponent();
+            DataContext = this;
 
-//    // דוגמאות לאוספים של enum להצגה בקומבו
-//    public ObservableCollection<Role> Roles { get; set; }
-//    public ObservableCollection<DistanceType> DistanceTypes { get; set; }
+            CurrentVolunteer = id != 0 ? s_bl.Volunteer.GetVolunteerDetails(id) : new BL.BO.Volunteer();
 
-//    public VolunteerWindow(int id = 0)
-//    {
-//        // יוזם את האובייקט לפני InitializeComponent
-//        Volunteer = new Volunteer();
-//        Roles = new ObservableCollection<Role>((Role[])Enum.GetValues(typeof(Role)));
-//        DistanceTypes = new ObservableCollection<DistanceType>((DistanceType[])Enum.GetValues(typeof(DistanceType)));
+            if (CurrentVolunteer!.VolunteerId != 0)
+            {
+                s_bl.Volunteer.AddObserver(CurrentVolunteer!.VolunteerId, RefreshCurrentStudent);
+            }
 
-//        ButtonText = id == 0 ? "Add" : "Update";
+            ButtonText = id == 0 ? "Add" : "Update";
 
-//        InitializeComponent();
+            this.Loaded += VolunteerWindow_Loaded;
+            this.Closing += VolunteerWindow_Closing;
+        }
+        private void VolunteerWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (CurrentVolunteer != null && CurrentVolunteer.VolunteerId != 0)
+            {
+                s_bl.Volunteer.AddObserver(CurrentVolunteer.VolunteerId, RefreshCurrentStudent);
+            }
+        }
 
-//        this.DataContext = this;
+        private void VolunteerWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (CurrentVolunteer != null && CurrentVolunteer.VolunteerId != 0)
+            {
+                s_bl.Volunteer.RemoveObserver(CurrentVolunteer.VolunteerId, RefreshCurrentStudent);
+            }
+        }
 
-//        if (id != 0)
-//        {
-//            // כאן יש לאכלס את ה-Volunteer מנתונים קיימים (לדוגמה BL)
-//            LoadVolunteer(id);
-//        }
-//    }
+        private void RefreshCurrentStudent()
+        {
+            int id = CurrentVolunteer!.VolunteerId;
+            CurrentVolunteer = null;
+            CurrentVolunteer = s_bl.Volunteer.GetVolunteerDetails(id);
+        }
 
-//    private void LoadVolunteer(int id)
-//    {
-//        // לדוגמה - כאן תטען את ה-Volunteer לפי id מתוך BL
-//        // Volunteer = yourBL.GetVolunteerById(id);
 
-//        // להדגמה, נניח ממלאים ידנית:
-//        Volunteer = new Volunteer
-//        {
-//            VolunteerId = id,
-//            Name = "Moshe",
-//            PhoneNumber = "050-1234567",
-//            EmailOfVolunteer = "moshe@example.com",
-//            PasswordVolunteer = "secret",
-//            AddressVolunteer = "Rehov Hagefen 12",
-//            VolunteerLatitude = 32.0853,
-//            VolunteerLongitude = 34.7818,
-//            IsAvailable = true,
-//            MaximumDistanceForReceivingCall = 10,
-//            Role = Role.Admin,
-//            DistanceType = DistanceType.Kilometers
-//        };
-//        OnPropertyChanged(nameof(Volunteer));
-//    }
+        private void btnAddUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (ButtonText == "Add")
+                {
+                    s_bl.Volunteer.AddVolunteer(CurrentVolunteer);
+                    MessageBox.Show("התנדבות נוספה בהצלחה!", "הצלחה", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else if (ButtonText == "Update")
+                {
+                    s_bl.Volunteer.UpdateVolunteer(CurrentVolunteer.VolunteerId, CurrentVolunteer);
+                    MessageBox.Show("התנדבות עודכנה בהצלחה!", "הצלחה", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("מצב כפתור לא ידוע.", "שגיאה", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
 
-//    private void btnAddUpdate_Click(object sender, RoutedEventArgs e)
-//    {
-//        if (ButtonText == "Add")
-//        {
-//            // הוספת וולונטר חדש
-//            MessageBox.Show("Adding new Volunteer...");
-//            // כאן תקרא לשכבת BL להוספה
-//        }
-//        else
-//        {
-//            // עדכון וולונטר קיים
-//            MessageBox.Show("Updating existing Volunteer...");
-//            // כאן תקרא לשכבת BL לעדכון
-//        }
-//    }
+                this.Close();
+            }
+            catch (BL.BO.BlDoesNotExistException ex)
+            {
+                MessageBox.Show(ex.Message, "שגיאה ב־BL", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("אירעה שגיאה לא צפויה: " + ex.Message, "שגיאה", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
 
-//    // לעדכון סיסמה ב-Volunteer.PasswordVolunteer
-//    private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
-//    {
-//        var passwordBox = sender as System.Windows.Controls.PasswordBox;
-//        if (passwordBox != null)
-//        {
-//            Volunteer.PasswordVolunteer = passwordBox.Password;
-//        }
-//    }
 
-//    public event PropertyChangedEventHandler? PropertyChanged;
-//    private void OnPropertyChanged(string propName)
-//    {
-//        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
-//    }
-//}
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
