85608ba5f310ca959437c80bee2baf0b06acf21e
RivkiPerkoff <Hand4d24@mbjcomp.org.il>
2025-06-23 14:55:47 +0300

    add call window

--- dotNet5785_5379_6612_0845/PL/Call/CallWindow.xaml.cs
+++ dotNet5785_5379_6612_0845/PL/Call/CallWindow.xaml.cs
@@ -1,27 +1,103 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using BL.BIApi;
+using BL.BO;
+using BlApi;
 
-namespace PL.Call
+namespace PL.Call;
+
+public partial class CallWindow : Window, INotifyPropertyChanged
 {
-    /// <summary>
-    /// Interaction logic for CallWindow.xaml
-    /// </summary>
-    public partial class CallWindow : Window
+    private static readonly IBL bl = BlApi.Factory.Get();
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected virtual void OnPropertyChanged(string name) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+    public string ButtonText
     {
-        public CallWindow()
+        get => (string)GetValue(ButtonTextProperty);
+        set
         {
-            InitializeComponent();
+            SetValue(ButtonTextProperty, value);
+            OnPropertyChanged(nameof(ButtonText));
+            OnPropertyChanged(nameof(IsEditMode));
+        }
+    }
+
+    public static readonly DependencyProperty ButtonTextProperty =
+        DependencyProperty.Register(nameof(ButtonText), typeof(string), typeof(CallWindow), new PropertyMetadata("Add"));
+
+    public bool IsEditMode => ButtonText == "Update";
+
+    public BL.BO.Call? CurrentCall
+    {
+        get => (BL.BO.Call?)GetValue(CurrentCallProperty);
+        set => SetValue(CurrentCallProperty, value);
+    }
+
+    public static readonly DependencyProperty CurrentCallProperty =
+        DependencyProperty.Register(nameof(CurrentCall), typeof(BL.BO.Call), typeof(CallWindow), new PropertyMetadata(null));
+
+    public CallWindow(int callId = 0)
+    {
+        InitializeComponent();
+        ButtonText = callId != 0 ? "Update" : "Add";
+
+        if (callId != 0)
+        {
+            try
+            {
+                var call = bl.Call.GetCallDetails(callId);
+                CurrentCall = call;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading call: " + ex.Message);
+                Close();
+            }
+        }
+        else
+        {
+            CurrentCall = new BL.BO.Call
+            {
+                IdCall = 0,
+                CallDescription = "",
+                AddressOfCall = "",
+                OpeningTime = DateTime.Now,
+                MaxFinishTime = DateTime.Now.AddHours(2),
+                CallType = CallTypes.None
+            };
+        }
+
+        DataContext = this;
+    }
+
+    private void btnAddUpdate_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            if (CurrentCall == null)
+                return;
+
+            if (ButtonText == "Add")
+            {
+                bl.Call.AddCall(CurrentCall);
+                MessageBox.Show("Call added successfully.");
+            }
+            else
+            {
+                bl.Call.UpdateCallDetails(CurrentCall);
+                MessageBox.Show("Call updated successfully.");
+            }
+
+            Close();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
